611e6f495e213eb9d11da3e0d7d78578db0d7749
James Ferguson <ncjamesferguson@gmail.com>
2017-12-21 23:56:22 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,11 @@
 class BankAccount
 
 attr_accessor :balance, :status
+
 attr_reader :name
+
   def initialize(name)
-    self.name = name
+    @name = name
     self.balance = 1000
     self.status = "open"
   end
