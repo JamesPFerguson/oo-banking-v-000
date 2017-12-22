7cde5a40defab3d31b1094523b6a56899cff92a9
James Ferguson <ncjamesferguson@gmail.com>
2017-12-22 00:01:17 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,9 +2,10 @@ require_relative 'bank_account.rb'
 
 class Transfer
 
-  attr_accessor :status
+  attr_accessor :status,
+  attr_reader :amount
 
   def initialize(sender, receiver, amount)
     @status = "pending"
-
+    @amount = amount
 end
