896fa8030ff8f7f32ed5ecec043db37876f6dc03
James Ferguson <ncjamesferguson@gmail.com>
2017-12-22 00:08:14 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end
 
   def execute_transaction
-    if self.status != "complete" && self.valid?
+    if self.status != "complete" && self.valid? && sender.balance > @amount
     sender.balance -= @amount
     receiver.balance += @amount
     self.status = "complete"
